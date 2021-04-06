9662d20b291f110a2e2c398e3c0ce42abb84f21e
dunnerjunge <57312980+dunnerjunge@users.noreply.github.com>
2021-04-06 10:44:52 +0200

    New Map
    upload Lobby map
    remove testmap

---
+++ tsx/desks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="desks" tilewidth="32" tileheight="32" tilecount="110" columns="10">
+ <image source="../workadventure-map-starter-kit-master/desks.png" width="320" height="352"/>
+</tileset>

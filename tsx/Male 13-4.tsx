fd3014d157dcb0ff7d966b376e5be37b0c9683a3
dunnerjunge <57312980+dunnerjunge@users.noreply.github.com>
2021-04-06 08:45:35 +0000

    Deploying to gh-pages from master @ 9662d20b291f110a2e2c398e3c0ce42abb84f21e 🚀

---
+++ tsx/Male 13-4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Male 13-4" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../workadventure-map-starter-kit-master/Male 13-4.png" width="96" height="128"/>
+</tileset>
---
+++ tsx/own.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="own" tilewidth="32" tileheight="32" tilecount="8393" columns="77">
+ <image source="../workadventure-map-starter-kit-master/own.png" width="2480" height="3508"/>
+</tileset>
